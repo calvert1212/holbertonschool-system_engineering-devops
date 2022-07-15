df20bec27e8305769c0e2136484216b97d79f38d
calvert1212 <calvert1212@gmail.com>
2022-07-14 23:57:53 -0500

    Update 100-puppet_ssh_config.pp

--- 0x00-ssh/100-puppet_ssh_config.pp
+++ 0x00-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,11 @@
-# sets up a client SSH config file so that we can connect to a server without using a password
-include stdlib
-file_line { 'Declare identity file':
+#!/usr/bin/env bash
+# connect with puppet
+file_line { 'Declare_identity_file':
   path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/school',
-  replace => true,
+  line    => 'IdentityFile ~/.ssh/holberton',
 }
 
-file_line { 'Turn off passwd auth':
+file_line { 'Turn_off_passwd_auth':
   path    => '/etc/ssh/ssh_config',
-  line    => '    PasswordAuthentication no',
-  replace => true,
-} 
+  line    => 'PasswordAuthentication no',
+}
