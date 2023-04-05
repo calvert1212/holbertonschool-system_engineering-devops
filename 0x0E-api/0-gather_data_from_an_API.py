e0dd49134d2bc9a081d1839371690884383144d2
calvert1212 <calvert1212@gmail.com>
2023-04-05 17:40:05 -0500

    Update 0-gather_data_from_an_API.py

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -1,44 +1,28 @@
 #!/usr/bin/python3
-"""
-This module retrieves TODO list progress of a given employee ID from a
-REST API.
-"""
-
+"""returns info about employee's todo list"""
 import requests
 from sys import argv
 
-if __name__ == '__main__':
-    if len(argv) != 2:
-        print("Usage: {} EMPLOYEE_ID".format(argv[0]))
-        exit(1)
 
-    EMPLOYEE_ID = argv[1]
-    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
-        EMPLOYEE_ID)
-    response = requests.get(url)
+if __name__ == "__main__":
+    # get the given employees info
+    employee = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+    ).json()
 
-    if response.status_code != 200:
-        print("Error: Request failed with status code {}".format(
-            response.status_code))
-        exit(1)
+    # get the employees todo list
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos".format(argv[1])
+    ).json()
 
-    todos = response.json()
-    total_tasks = len(todos)
-    done_tasks = [todo for todo in todos if todo['completed']]
+    # make it an actual list object
+    todo_list = [
+        task.get("title") for task in todos if task.get("completed") is True
+    ]
 
-    employee_name_url = "https://jsonplaceholder.typicode.com/users/{}".format(
-        EMPLOYEE_ID)
-    response_name = requests.get(employee_name_url)
-
-    if response_name.status_code != 200:
-        print("Error: Request failed with status code {}".format(
-            response_name.status_code))
-        exit(1)
-
-    employee_name = response_name.json().get('name')
-
-    print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, len(done_tasks), total_tasks))
-
-    for todo in done_tasks:
-        print("\t {} {}".format('\t', todo['title']))
+    # format and print response
+    print(
+        "Employee {} is done with tasks({}/{}):".format(
+            employee.get("name"), len(todo_list), len(todos)
+        ), *todo_list, sep="\n\t "
+    )
